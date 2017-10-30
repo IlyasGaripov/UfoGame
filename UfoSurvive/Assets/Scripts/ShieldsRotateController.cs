5b77df7d13b0e979815fbc176152540ced2b2c34
Garipov Ilyas <garillas.feedback@gmail.com>
2017-10-30 22:17:32 +0300

    prototype_test_2_10_30_2017

--- UfoSurvive/Assets/Scripts/ShieldsRotateController.cs
+++ UfoSurvive/Assets/Scripts/ShieldsRotateController.cs
@@ -2,32 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShieldsRotateController : MonoBehaviour {
+public class ShieldsRotateController : MonoBehaviour
+{
+    public int rotationDirection; // -1 for clockwise
+                                       //  1 for anti-clockwise
 
-    private bool blockMove = false;
+    public int rotationStep = 5;    // should be less than 90
+    public bool rotateFlag = true;
+ 
+    // All the objects with which collision will be checked
+    public GameObject[] objectsArray;
 
-    // Use this for initialization
-    void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private Vector3 currentRotation, targetRotation;
 
-        if (Input.GetKeyDown("e"))
-        {
-            
-        }
-        if (Input.GetKeyDown("q"))
-        {
-            
-        }
+    public float timeBetweenShots = 0.01f;  // Allow 3 shots per second
 
-    }
+    private float timestamp;
 
-    public void RotateMe()
+    private void rotateObject()
     {
-        //if (blockMove == false)
-                //blockMove = true;
-          //  blockMove = false;
+        currentRotation = gameObject.transform.eulerAngles;
+        targetRotation.y = (currentRotation.y + (90 * rotationDirection));
+        StartCoroutine(objectRotationAnimation());
+
     }
+
+    IEnumerator objectRotationAnimation()
+    {
+        // add rotation step to current rotation.
+        currentRotation.y += (rotationStep * rotationDirection);
+        gameObject.transform.eulerAngles = currentRotation;
+        yield return new WaitForSeconds(0);
+        if (((int)currentRotation.y >(int)targetRotation.y && rotationDirection < 0) ||  ((int)currentRotation.y < (int)targetRotation.y && rotationDirection > 0)) 
+        {
+                StartCoroutine(objectRotationAnimation());        
+        }
+        else
+        {
+            gameObject.transform.eulerAngles = targetRotation;
+        }
+    }
+    IEnumerator rotateObjectAgain()
+    {
+        yield return new WaitForSeconds(0.2f);
+        rotateObject();
+    }
+
+
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("e") &&  Time.time >= timestamp)
+        {
+            rotationDirection = 1;
+            rotateObject();
+            timestamp = Time.time + timeBetweenShots;
+        }
+
+        if (Input.GetKeyDown("q") && Time.time >= timestamp)
+        {
+            rotationDirection = -1;
+            rotateObject();
+            timestamp = Time.time + timeBetweenShots;
+        }
+    }
+
+
 }
