4bad75e81e96c98088366ee2c23efe991ff7c2df
Garipov Ilyas <garillas.feedback@gmail.com>
2017-10-30 19:06:19 +0300

    prototype_test_30_10_2017

---
+++ UfoSurvive/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+    private bool rotating = true;
+    public float speed;
+    private int angle;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, 1));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -1));
+        }
+
+    }
+}
---
+++ UfoSurvive/Assets/Scripts/ShieldsRotateController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldsRotateController : MonoBehaviour {
+
+    private bool blockMove = false;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("e"))
+        {
+            
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            
+        }
+
+    }
+
+    public void RotateMe()
+    {
+        //if (blockMove == false)
+                //blockMove = true;
+          //  blockMove = false;
+    }
+}
