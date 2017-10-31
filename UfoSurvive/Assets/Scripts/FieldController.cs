96ced7e8399787c8fc69a502134630d2f018cc0e
Garipov Ilyas <garillas.feedback@gmail.com>
2017-10-31 13:40:56 +0300

    prototype_test_10_31_2017
    add bullet controlller

---
+++ UfoSurvive/Assets/Scripts/FieldController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if(other.tag == "Bullet")
+        {
+            Destroy(other);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- UfoSurvive/Assets/Scripts/MovementScript.cs
+++ UfoSurvive/Assets/Scripts/MovementScript.cs
@@ -6,20 +6,27 @@ public class MovementScript : MonoBehaviour {
     private bool rotating = true;
     public float speed;
     private int angle;
+
+    public float smoothing;
+    private Vector3 normalizeDirection;
+
     // Use this for initialization
-    void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey(KeyCode.W))
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(new Vector3(0, 0, 1));
         }
+        //transform.Translate(new Vector3(0, 0, 1));
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(new Vector3(0, 0, -1));
         }
-
     }
+
+
 }
